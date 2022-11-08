aa49c3adc7c775b42d2b9e07acb2bac4d1181a7e
sinmincheol <smc9919@naver.com>
2022-11-08 12:43:46 +0900

    Merged RFC body field

--- src/SendingMail.java
+++ src/SendingMail.java
@@ -1,4 +1,3 @@
-import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import javax.net.ssl.*;
 import java.io.*;
@@ -19,13 +18,14 @@ public class SendingMail {
     //사용자 입력 받기
     public static void requestInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ID (e-mail):");
+        System.out.print("ID:");
         user = scanner.next();
-        while (!user.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?")){ //메일 형식일 때까지
-            System.out.println("메일 형식이 아닙니다. 다시 입력하세요.");
-            System.out.print("ID (e-mail):");
+        while (!(user.contains("@naver.com"))) { //메일 형식이 @naver.com일 때까지
+            System.out.println("메일 형식이 다릅니다. 다시 입력하세요.");
+            System.out.print("ID:");
             user = scanner.next();
         }
+
         System.out.print("Password:");
         pass = scanner.next();
         System.out.print("Receiver:");
@@ -41,81 +41,66 @@ public class SendingMail {
             String tmp = scanner.nextLine();
             if (tmp.equals("end!")) //end! 입력 시 종료
                 break;
-            body += tmp; //입력받은 한 라인 씩 body 에 추가
+            body += tmp; //입력받은 한 라인 씩 body에 추가
             body += "\r\n"; //줄바꿈 개행문자 추가
         }
+
+
+
     }
 
     public static void main(String[] args) throws Exception {
+
+        int delay = 1000;
         String charSet = "UTF-8" ;
-        SSLSocketFactory sslSocketFactory;
-        SSLSocket sslSocket = null;
 
         requestInput(); //사용자 입력 메서드 호출
 
         String username = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
         String password = Base64.getEncoder().encodeToString(pass.getBytes(StandardCharsets.UTF_8));
-        String nick = new String(nickname.getBytes(charSet), StandardCharsets.ISO_8859_1);
-        String sub = new String(subject.getBytes(charSet), StandardCharsets.ISO_8859_1);
-        String emailBody = new String(body.getBytes(charSet), StandardCharsets.ISO_8859_1);
-        String stmpName = user.split("@")[1];
+        String nick = new String(nickname.getBytes(charSet), "8859_1");
+        String sub = new String(subject.getBytes(charSet), "8859_1");
+        String emailbody = new String(body.getBytes(charSet), "8859_1");
+        String msg = "";
 
-        try {
-            sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            sslSocket = (SSLSocket) sslSocketFactory.createSocket("smtp." + stmpName, 465);
-        } catch (UnknownHostException e) {
-            System.out.println("지원하지 않는 이메일 입니다." + stmpName);
-            System.exit(1);
-        }
+        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket("smtp.naver.com", 465);
 
         br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
 
         dataOutputStream = new DataOutputStream(sslSocket.getOutputStream());
 
-        System.out.println("-------------EHLO------------");
-        send("EHLO smtp." + stmpName + "\r\n",7);
-        System.out.println("------------AUTH LOGIN-------------");
-        send("AUTH LOGIN\r\n",1);
-        System.out.println("-------------USER------------");
-        send(username+"\r\n",1);
-        System.out.println("-------------PASS------------");
-        send(password+"\r\n",1);
-        System.out.println("-------------MAIL FROM------------");
-        send("MAIL FROM:<" + user + ">\r\n",1);
-        System.out.println("-------------RCPT TO------------");
-        send("RCPT TO:<" + receiver + ">\r\n",1);
-        System.out.println("-------------DATA------------");
-        send("DATA\r\n",1);
-        System.out.println("-------------FROM------------");
-        send("From: "+ nick + " <" + user + ">\r\n", 0);
-        System.out.println("-------------Subject------------");
-        send("Subject: " + sub + "\r\n",0);
-        System.out.println("----------------To--------------");
-        send("To: "+ receiver + "\r\n", 0);
-        System.out.println("-------------Email Body------------");
-        send("\r\n"+ emailBody + "\r\n",0);
-        System.out.println("-------------Content------------");
-        send(".\r\n",0);
-        System.out.println("-------------QUIT------------");
-        send("QUIT\r\n",1);
+        msg += "EHLO smtp.naver.com\r\n";
+        msg += "AUTH LOGIN\r\n";
+        msg += (username + "\r\n");
+        msg += (password + "\r\n");
+        msg += ("MAIL FROM:<" + user + ">\r\n");
+        msg += ("RCPT TO:<" + receiver + ">\r\n");
+        msg += "DATA\r\n";
+        msg += ("From: "+ nick + " <" + user + ">\r\n");
+        msg += ("Subject: " + sub + "\r\n");
+        msg += ("To: "+ receiver + "\r\n");
+        msg += ("\r\n"+ emailbody + "\r\n");
+        msg += ".\r\n";
+        msg += "QUIT\r\n";
+
+        send(msg, 14);
 
         sslSocket.close();
     }
     private static void send(String s, int no_of_response_line) throws Exception
     {
-        int delay = 1000;
-
         dataOutputStream.writeBytes(s);
         System.out.println("CLIENT: "+s);
-        Thread.sleep(delay);
-        String login;
+        Thread.sleep(1000);
+        String login = null;
 
         for (int i = 0; i < no_of_response_line; i++) {
             login = br.readLine();
             System.out.println("SERVER : " + login);
 
-            if (login == null || login.contains("Username and Password not accepted")) {
-                System.out.println("로그인 실패.");
+            if (login.contains("Username and Password not accepted")) {
+                System.out.println("로그인이 안 됩니다.");
                 System.exit(0);
             }
         }
